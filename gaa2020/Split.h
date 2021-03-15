69a13ec6a7b651ea2274e692e7623cccd700dc15
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-03-15 21:58:18 +0300

    cw

---
+++ gaa2020/Split.h
@@ -0,0 +1,8 @@
+
+#pragma once
+#include "stdafx.h"
+
+#define max_word 500
+#define size_word 50
+
+char** SplitText(char source[], int size);
---
+++ gaa2020/stdafx.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma warning(disable:4996)
+
+#include <locale>	
+#include <tchar.h>
+
+#include <iostream>
+#include <fstream>
